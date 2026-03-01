188c8c7137ae82294b1b21e53712ac4114b6a534
Jaejun <loveayase@gmail.com>
2026-03-01 17:18:31 +0900

    4-3 (1) 어떻게 도형을 움직일까?

---
+++ mouse_circle_my_version.py
@@ -0,0 +1,29 @@
+'''
+ * Project Name: python_game
+ * NAME: 
+ * Made by Jaejun
+ * Date: 26. 3. 1.
+ * Desc: 
+'''
+'''
+ * Project Name: python_game
+ * NAME: 
+ * Made by Jaejun
+ * Date: 26. 3. 1.
+ * Desc: 
+'''
+
+import tkinter
+
+
+def move(e):
+    cvs.delete('all')
+    cvs.create_oval(e.x - 25, e.y - 25, e.x + 25, e.y + 25, fill="blue", outline="cyan")
+
+
+root = tkinter.Tk()
+root.title("커서를 따라 다니는 도형")
+root.bind("<Motion>", move)
+cvs = tkinter.Canvas(width=800, height=400)
+cvs.pack()
+root.mainloop()
