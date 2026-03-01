04aca79b1b0c7570afc21404003c60c3beac1bbc
Jaejun <loveayase@gmail.com>
2026-03-01 16:28:43 +0900

    3-5 (2) 이미지 표시

--- canvas_image.py
+++ canvas_image.py
@@ -11,7 +11,6 @@ import tkinter
 root = tkinter.Tk()
 root.title("캔버스에 이미지를 표시해보자")
 cvs = tkinter.Canvas(width=1080, height=720)
-cvs.create_line(0, 0, 100, 160, 200, 20, 300, 60, smooth=True)
 img = tkinter.PhotoImage(file="./image/chap3_illust.png")
 cvs.create_image(540, 360, image=img)
 cvs.pack()
