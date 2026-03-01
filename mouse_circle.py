fd6ac1cf0b81bf97da7a0fbf8e6a383dd39220e4
Jaejun <loveayase@gmail.com>
2026-03-01 20:13:53 +0900

    4-3 (2) 원이 커서를 따라가는 프로그램(보다 자연스러운 움직임)

--- mouse_circle.py
+++ mouse_circle.py
@@ -3,10 +3,12 @@
  * NAME: 
  * Made by Jaejun
  * Date: 26. 3. 1.
- * Desc: 
+ * Desc: 전역 변수를 global을 이용해서 코딩하는것의 장점이 무엇인가?
+ 내가 너무 객체지향적으로 생각하는 것일까
 '''
-
+import math
 import tkinter
+from multiprocessing.sharedctypes import class_cache
 
 mx = 400
 my = 300
@@ -21,14 +23,31 @@ def move(e):
 cx = 400
 cy = 300
 cr = 50
+distance = [5, 5]
+
+
+def calc_distance():
+    # 좀더 자연스럽게 움직이도록 해보자
+    global cx, cy, distance
+
+    x = abs(mx - cx)
+    y = abs(my - cy)
+    dis = math.sqrt(x * x + y * y)
+    if dis == 0:
+        dis = 1
+
+    distance[0] = (y / dis) * 5
+    distance[1] = (x / dis) * 5
+
+    if cy > my: cy -= distance[0]
+    if cy < my: cy += distance[0]
+    if cx > mx: cx -= distance[1]
+    if cx < mx: cx += distance[1]
 
 
 def main():
     global cx, cy
-    if cy > my: cy -= 5
-    if cy < my: cy += 5
-    if cx > mx: cx -= 5
-    if cx < mx: cx += 5
+    calc_distance()
     cvs.delete('all')
     cvs.create_oval(cx - cr, cy - cr, cx + cr, cy + cr, fill="blue", outline="cyan")
 
