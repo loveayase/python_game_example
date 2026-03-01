88204ae237a49cdb10079ecfc2f4a032e2f0a0af
Jaejun <loveayase@gmail.com>
2026-03-01 15:37:02 +0900

    Initial commit

---
+++ window_1.py
@@ -0,0 +1,14 @@
+'''
+ * Project Name: python_game
+ * NAME: 
+ * Made by Jaejun
+ * Date: 26. 3. 1.
+ * Desc: 
+'''
+
+import tkinter
+
+root = tkinter.Tk()
+root.geometry("800x480")
+root.title("타이틀")
+root.mainloop()
