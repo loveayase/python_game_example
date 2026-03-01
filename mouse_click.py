1fdd86269442dda9eafd50aa4e2a46631f4ae2ce
Jaejun <loveayase@gmail.com>
2026-03-01 17:04:34 +0900

    4-2 (6) 마우스를 클릭한 사실을 알아내는 방법은?

---
+++ mouse_click.py
@@ -0,0 +1,32 @@
+'''
+ * Project Name: python_game
+ * NAME: 
+ * Made by Jaejun
+ * Date: 26. 3. 1.
+ * Desc: 
+'''
+
+import tkinter
+
+FNT = ("Times New Roman", 40)
+
+
+def move(e):
+    cvs.delete('all')
+    s = "({}, {})".format(e.x, e.y)
+    cvs.create_text(400, 200, text=s, font=FNT)
+
+
+def click(e):
+    cvs.delete('all')
+    s = "Click! ({}, {})".format(e.x, e.y)
+    cvs.create_text(400, 200, text=s, font=FNT)
+
+
+root = tkinter.Tk()
+root.title("마우스 커서 좌표")
+root.bind("<Motion>", move)
+root.bind("<Button>", click)
+cvs = tkinter.Canvas(width=800, height=400)
+cvs.pack()
+root.mainloop()
